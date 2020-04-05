1c740912c902baa8878adaad9ec8f3d485fd0b45
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-05 10:52:12 +0200

    :construction: add function definition

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,19 +1,22 @@
 let weight = 55;
 let height = 1.67;
 const BMI = weight / (height * height);
-let result; 
 
 
-if (BMI < 18.5) {
-    result = `Your BMI is ${BMI}, this means that you are underweight`;
-} else if (BMI > 18.5 && BMI < 25) {
-    result = `Your BMI is ${BMI}, this means that you have a normal weight`;
-} else if (BMI > 25 && BMI < 30) {
-    result = `Your BMI is ${BMI}, this means that you are overweight`;
-} else if (BMI > 30) {
-    result = `Your BMI is ${BMI}, this means that you are obese`;
-} else {
-    result = 'Sorry, something went wrong. Try again later.'; 
+function getBmiResult(bmiResult){
+    let result; 
+    
+    if (bmiResult < 18.5) {
+        result = `Your BMI is ${bmiResult}, this means that you are underweight`;
+    } else if (bmiResult > 18.5 && bmiResult < 25) {
+        result = `Your bmiResult is ${bmiResult}, this means that you have a normal weight`;
+    } else if (bmiResult > 25 && bmiResult < 30) {
+        result = `Your bmiResult is ${bmiResult}, this means that you are overweight`;
+    } else if (bmiResult > 30) {
+        result = `Your BMI is ${bmiResult}, this means that you are obese`;
+    } else {
+        result = 'Sorry, something went wrong. Try again later.'; 
+    }
+
+    return result;
 }
-
-console.log(result)
