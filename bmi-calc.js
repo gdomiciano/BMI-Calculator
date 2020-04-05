b580b566f0e8b8e2375a2a26819230ed3c8e10c2
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-05 10:29:16 +0200

    :construction: add condition to return result to BMI

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,6 +1,19 @@
 let weight = 55;
 let height = 1.67;
-const formula = weight / (height * height);
+const BMI = weight / (height * height);
 let result; 
 
-console.log(formula)
+
+if (BMI < 18.5) {
+    result = `Your BMI is ${BMI}, this means that you are underweight`;
+} else if (BMI > 18.5 && BMI < 25) {
+    result = `Your BMI is ${BMI}, this means that you have a normal weight`;
+} else if (BMI > 25 && BMI < 30) {
+    result = `Your BMI is ${BMI}, this means that you are overweight`;
+} else if (BMI > 30) {
+    result = `Your BMI is ${BMI}, this means that you are obese`;
+} else {
+    result = 'Sorry, something went wrong. Try again later.'; 
+}
+
+console.log(result)
