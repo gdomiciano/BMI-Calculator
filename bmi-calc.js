9f09d027cbf2caa0d3d34b2fac5891a7d03787fb
Geisy Domiciano <geisydomiciano@gmail.com>
2020-04-09 20:21:05 +0200

    :construction: add functions to integrate with users

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,19 +1,31 @@
-const height = parseFloat(prompt("Insert your height (m):"));
-const weight = parseFloat(prompt("Insert your weight (kg):"));
-const BMI = weight / (height * height);
 
-let result;
-	
-if (BMI < 18.5) {
-    result = `Your BMI is ${BMI}, this means that you are underweight`;
-} else if (BMI > 18.5 && BMI < 25) {
-    result = `Your BMI is ${BMI}, this means that you have a normal weight`;
-} else if (BMI > 25 && BMI < 30) {
-    result = `Your BMI is ${BMI}, this means that you are overweight`;
-} else if (BMI > 30) {
-    result = `Your BMI is ${BMI}, this means that you are obese`;
-} else {
-    result = 'Sorry, something went wrong. Try again later.'; 
-}
+(
+    function () {
+        const height = parseFloat(prompt("Insert your height (m):"));
+        const weight = parseFloat(prompt("Insert your weight (kg):"));
 
-console.log(result)
+        const calculateBmi = function(w, h) {
+            return w / h**2
+        }
+
+        function getBmiResult(BMI) {
+            let result = 'Your BMI is';
+            
+            if (BMI < 18.5) {
+                result = `${result} ${BMI}, this means that you are underweight`;
+            } else if (BMI > 18.5 && BMI < 25) { 
+                result = `${result} ${BMI}, this means that you have a normal weight`;
+            } else if (BMI > 25 && BMI < 30) {
+                result = `${result} ${BMI}, this means that you are overweight`;
+            } else if (BMI > 30) {
+                result = `${result} ${BMI}, this means that you are obese`;
+            } else {
+                result = 'Sorry, something went wrong. Try again later.'; 
+            }
+
+            console.log(result)
+        }
+
+        getBmiResult(calculateBmi(weight, height));
+    }
+)();
