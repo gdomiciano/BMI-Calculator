3c9c75a2cbd39ee45a57a7d3cd0a3591f5652a0c
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-09 16:50:55 +0200

    :sparkles: show result

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,25 +1,25 @@
 const calculateBmi = function () { 
     let weight = document.getElementById('weight').value;
     let height = document.getElementsByClassName('field-height')[0].value;
-    
+
     return weight / (height**2) 
 };
 
 function getBmiClass () {
-    const bmiResult = calculateBmi();
+    const bmiResult = calculateBmi().toFixed(2);
     let result; 
 
     if (bmiResult < 18.5) {
         result = `Your BMI is ${bmiResult}, this means that you are underweight`;
     } else if (bmiResult > 18.5 && bmiResult < 25) {
-        result = `Your bmiResult is ${bmiResult}, this means that you have a normal weight`;
+        result = `Your BMI is ${bmiResult}, this means that you have a normal weight`;
     } else if (bmiResult > 25 && bmiResult < 30) {
-        result = `Your bmiResult is ${bmiResult}, this means that you are overweight`;
+        result = `Your BMI is ${bmiResult}, this means that you are overweight`;
     } else if (bmiResult > 30) {
         result = `Your BMI is ${bmiResult}, this means that you are obese`;
     } else {
         result = 'Sorry, something went wrong. Try again later.'; 
     }
 
-    console.log(result);
+    document.querySelector('.result').textContent = result
 }
