65458fc5fd412341124898861c946312e84f91f3
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-04-11 10:24:37 +0200

    :sparkles: using object to render information in the page via for in loop

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,49 +1,59 @@
 //create basic Object
 let bmiCalculator = {
-    title: 'BMI Calculator',
-    introduction: '<strong>Body Mass Index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.'
-}
+    elementsObj: {
+        title: 'BMI Calculator',
+        intro: '<strong>Body Mass Index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.',
+    },
 
-// replace simple string content using object
-let $title = document.querySelector('.title')
-$title.textContent = bmiCalculator.title
-
-// replace raw HTML using insertAdjacentHTML
-let $intro = document.querySelector('.intro')
-console.log({$intro}) // check available methods inside the element
-$intro.insertAdjacentHTML('afterbegin', bmiCalculator.introduction)
-
-
-const calculateBmi = function() {
+    // add methods
+    calculateBmi() {
         const height = document.getElementById('height').value; // select the element based on the ID
-        console.log(height)
         const weight = document.getElementsByClassName('field-weight')[0].value; // select the elements based on class name
-        console.log(weight)
-    return weight / height**2;
+        return weight / height**2;
+    },
+
+    getBmiResult() {
+        // use this to access another property ot method from th object
+        // this === bmiCalculator
+        const BMI = this.calculateBmi();
+        let result = 'Your BMI is';
+        let color = 'black';
+    
+        if (BMI < 18.5) {
+            color = 'red'
+            result = `${result} ${BMI}, this means that you are underweight`;
+        } else if (BMI > 18.5 && BMI < 25) { 
+            color = 'green'
+            result = `${result} ${BMI}, this means that you have a normal weight`;
+        } else if (BMI > 25 && BMI < 30) {
+            color = 'orange'
+            result = `${result} ${BMI}, this means that you are overweight`;
+        } else if (BMI > 30) {
+            color = 'red' 
+            result = `${result} ${BMI}, this means that you are obese`;
+        } else {
+            result = 'Sorry, something went wrong. Try again later.'; 
+        }
+    
+        let p = document.querySelector('.result');
+        p.textContent = result;
+        p.style.color = color;
+    }
 }
 
-function getBmiResult() {
-    const BMI = calculateBmi();
-    let result = 'Your BMI is';
-    let color = 'black';
+console.log(bmiCalculator.elementsObj)
+for (const element in bmiCalculator.elementsObj) {
+    console.log(element)
 
-    if (BMI < 18.5) {
-        color = 'red'
-        result = `${result} ${BMI}, this means that you are underweight`;
-    } else if (BMI > 18.5 && BMI < 25) { 
-        color = 'green'
-        result = `${result} ${BMI}, this means that you have a normal weight`;
-    } else if (BMI > 25 && BMI < 30) {
-        color = 'orange'
-        result = `${result} ${BMI}, this means that you are overweight`;
-    } else if (BMI > 30) {
-        color = 'red' 
-        result = `${result} ${BMI}, this means that you are obese`;
-    } else {
-        result = 'Sorry, something went wrong. Try again later.'; 
+    if (bmiCalculator.elementsObj.hasOwnProperty(element)) {
+
+        const $htmlElement = document.querySelector(`.${element}`);
+        // check if it is raw html content
+        if(bmiCalculator.elementsObj[element].includes('</')) {
+            $htmlElement.insertAdjacentHTML('afterbegin', bmiCalculator.elementsObj[element]);
+        } else {
+            $htmlElement.textContent = bmiCalculator.elementsObj[element];
+        }
+
     }
-
-    let p = document.querySelector('.result');
-    p.textContent = result;
-    p.style.color = color;
 }
