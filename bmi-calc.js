b21e25b85094b7aff3b37e9021859df98a60111d
Geisy Domiciano <geisydomiciano@gmail.com>
2020-04-14 19:57:25 +0200

    :art: add object and methods to render the page and deal with behaviour

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,40 +1,59 @@
+// create an object called bmiCalculator
+const bmiCalculator = {
+    // create a title property and assign a string as a value
+    title: 'BMI Calcuator',
+    // create a intro property and assign a string containing raw HTML as a value
+    intro: '<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.',
 
-// (
-    //     function () {
+    //add methods
+    calculateBmi() {
+        const height = document.getElementById('height').value; // select the element based on the ID
+        const weight = document.getElementsByClassName('field-weight')[0].value; // select the elements based on class name
+        return weight / height**2;
+    },
+
+    getBmiResult() {
+        // debugger;
+        //this === bmiCalculator
+        const BMI = this.calculateBmi();
+        let result = 'Your BMI is';
+        let color = 'black';
     
-        const calculateBmi = function() {
-                const height = document.getElementById('height').value; // select the element based on the ID
-                console.log(height)
-                const weight = document.getElementsByClassName('field-weight')[0].value; // select the elements based on class name
-                console.log(weight)
-            return weight / height**2;
+        if (BMI < 18.5) {
+            color = 'red'
+            result = `${result} ${BMI}, this means that you are underweight`;
+        } else if (BMI > 18.5 && BMI < 25) { 
+            color = 'green'
+            result = `${result} ${BMI}, this means that you have a normal weight`;
+        } else if (BMI > 25 && BMI < 30) {
+            color = 'orange'
+            result = `${result} ${BMI}, this means that you are overweight`;
+        } else if (BMI > 30) {
+            color = 'red' 
+            result = `${result} ${BMI}, this means that you are obese`;
+        } else {
+            result = 'Sorry, something went wrong. Try again later.'; 
         }
+    
+        let p = document.querySelector('.result');
+        p.textContent = result;
+        p.style.color = color;
+    }
+}
 
-        function getBmiResult() {
-            const BMI = calculateBmi();
-            let result = 'Your BMI is';
-            let color = 'black';
+// get the h1 element based on the classname from the DOM and assign it to a $title constant
+const $title = document.querySelector('.title')
 
-            if (BMI < 18.5) {
-                color = 'red'
-                result = `${result} ${BMI}, this means that you are underweight`;
-            } else if (BMI > 18.5 && BMI < 25) { 
-                color = 'green'
-                result = `${result} ${BMI}, this means that you have a normal weight`;
-            } else if (BMI > 25 && BMI < 30) {
-                color = 'orange'
-                result = `${result} ${BMI}, this means that you are overweight`;
-            } else if (BMI > 30) {
-                color = 'red' 
-                result = `${result} ${BMI}, this means that you are obese`;
-            } else {
-                result = 'Sorry, something went wrong. Try again later.'; 
-            }
+// assign the value from the title property from the bmiCalcuator object to the text xcontent og the h1's element
+$title.textContent = bmiCalculator.title
 
-            let p = document.querySelector('.result');
-            p.textContent = result;
-            p.style.color = color;
-        }
 
-//     }
-// )();
+// get the p element based on the classname from the DOM and assign it to a $intro constant
+const $intro = document.querySelector('.intro')
+
+//add the value from the intro property to the $intro element using the insertAdjacentHTML method available to any html element inside the DOM 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+$intro.insertAdjacentHTML('afterbegin', bmiCalculator.intro);
+
+
+
