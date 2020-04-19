a0123af326fee170b4590727957858e84001a516
Geisy Domiciano <geisydomiciano@gmail.com>
2020-04-19 10:28:34 +0200

    :art: optimize code with for in loop

--- bmi-calc.js
+++ bmi-calc.js
@@ -1,12 +1,15 @@
 // create an object called bmiCalculator
 const bmiCalculator = {
-    // create a title property and assign a string as a value
-    title: 'BMI Calcuator',
-    // create a intro property and assign a string containing raw HTML as a value
-    intro: '<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.',
+    elementsObj: {
+        // create a title property and assign a string as a value
+        title: 'BMI Calcuator',
+        // create a intro property and assign a string containing raw HTML as a value
+        intro: '<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.',
+        'long-desc': '<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres.<strong>Body mass index (BMI)</strong> is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m<sup>2</sup>, resulting from mass in kilograms and height in metres. '
+    },
 
     //add methods
-    calculateBmi() {
+    calculateBmi: () => {
         const height = document.getElementById('height').value; // select the element based on the ID
         const weight = document.getElementsByClassName('field-weight')[0].value; // select the elements based on class name
         return weight / height**2;
@@ -41,19 +44,38 @@ const bmiCalculator = {
     }
 }
 
-// get the h1 element based on the classname from the DOM and assign it to a $title constant
-const $title = document.querySelector('.title')
+// DRY - don't repreat yourself
+console.log(bmiCalculator)
+for (const element in bmiCalculator.elementsObj) {
+    obj = bmiCalculator.elementsObj
+    if (obj.hasOwnProperty(element)) {
+        
+        const $htmlEl = document.querySelector(`.${element}`);
+        const value = obj[element]
 
-// assign the value from the title property from the bmiCalcuator object to the text xcontent og the h1's element
-$title.textContent = bmiCalculator.title
+        if(value.includes('</')) {
+            $htmlEl.insertAdjacentHTML('afterbegin', value);
+        } else {
+            $htmlEl.textContent = value
+        }
+    }
+}
 
 
-// get the p element based on the classname from the DOM and assign it to a $intro constant
-const $intro = document.querySelector('.intro')
+// -------- can be removed from below, it is here fore reference ----------- //
+// // get the h1 element based on the classname from the DOM and assign it to a $title constant
+// const $title = document.querySelector('.title')
 
-//add the value from the intro property to the $intro element using the insertAdjacentHTML method available to any html element inside the DOM 
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
-$intro.insertAdjacentHTML('afterbegin', bmiCalculator.intro);
+// // assign the value from the title property from the bmiCalcuator object to the text xcontent og the h1's element
+// $title.textContent = bmiCalculator.title
+
+
+// // get the p element based on the classname from the DOM and assign it to a $intro constant
+// const $intro = document.querySelector('.intro')
+
+// //add the value from the intro property to the $intro element using the insertAdjacentHTML method available to any html element inside the DOM 
+// // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+// $intro.insertAdjacentHTML('afterbegin', bmiCalculator.intro);
 
 
 
